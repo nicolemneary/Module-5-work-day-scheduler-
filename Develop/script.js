d1b6b1cd08a8388fee53528f0e3b9998eaf25fd1
nicolemneary <143215103+nicolemneary@users.noreply.github.com>
2023-10-26 13:42:40 -0700

    last edit

--- Develop/script.js
+++ Develop/script.js
@@ -1,126 +1,51 @@
 
-var today = moment().format('MMMM Do YYYY');
 
-var now = moment().format('H A');
+var today = moment().format("dddd, MMMM Do YYYY ");
+
+var now = moment().format("H A");
+
+// current day
 
 $("#currentDay").text(today);
 
 
-var currentDay = [
-    {
-        id: "0",
-        hour: "09",
-        time: "09",
-        meridiem: "am",
-        reminder: "",
-    },
-    {
-        id: "1",
-        hour: "10",
-        time: "10",
-        meridiem: "am",
-        reminder: "",
-    },
+/* planWorkday entries for each hour of the workday */
+var planWorkday = [
+    { time: "9 AM", 
+        event: "" },
+    { time: "10 AM", 
+        event: "" },
+    { time: "11 AM", 
+        event: "" },
+    { time: "12 PM", 
+        event: "" },
+    { time: "1 PM", 
+        event: "" },
+    { time: "2 PM", 
+        event: "" },
+    { time: "3 PM", 
+        event: "" },
+    { time: "4 PM", 
+        event: "" },
+    { time: "5 PM", 
+        event: "" },
+  ];
 
-    {
-        id: "2",
-        hour: "11",
-        time: "11",
-        meridiem: "am",
-        reminder: "",
-    },
-
-    {
-        id: "3",
-        hour: "12",
-        time: "12",
-        meridiem: "pm",
-        reminder: "",
-    },
-
-    {
-        id: "4",
-        hour: "01",
-        time: "13",
-        meridiem: "pmm",
-        reminder: "",
-    },
-    {
-
-        id: "5",
-        hour: "02",
-        time: "14",
-        meridiem: "pm",
-        reminder: "",
-    },
-
-    {
-
-        id: "6",
-        hour: "03",
-        time: "15",
-        meridiem: "pm",
-        reminder: "",
-    },
-
-    {
-
-        id: "7",
-        hour: "04",
-        time: "16",
-        meridiem: "pm",
-        reminder: "",
-    },
-
-
-    {
-
-        id: "8",
-        hour: "05",
-        time: "17",
-        meridiem: "pm",
-        reminder: "",
-    },
-]
-
-function getHeaderDate() {
-    var currentHeaderDate = moment().format('dddd, MMMM Do');
-    $("#currentDay").text(currentHeaderDate);
-}
-
-function saveReminders() {
-    localStorage.setItem("currentDay", JSON.stringify(myDay));
-}
-
-function displayReminders() {
-    myDay.forEach(function (_thisHour) {
-        $(`#${_thisHour.id}`).val(_thisHour.reminder);
-    })
-}
-
-function init() {
-    var storedDay = JSON.parse(localStorage.getItem("currentDay"));
-
-    if (storedDay) {
-        myDay = storedDay;
-    }
-
-    saveReminders();
-    displayReminders();
-}
-
+/* Local Storage check */
 var workEvents = JSON.parse(localStorage.getItem("workDay"));
 if (workEvents) {
-    planWorkday = workEvents;
+  planWorkday = workEvents;
 }
 
+/* Current Day */
 $("#currentDay").text(today);
 
+/* Create rows */
 planWorkday.forEach(function(timeBlock, index) {
-    var timeLabel = timeBlock.time;
-    var BlockColor = colorRow(timeLabel);   
-    var row =
-    '<div class="time-block" id="' +
+	var timeLabel = timeBlock.time;
+	var blockColor = colorRow(timeLabel);
+	var row =
+		'<div class="time-block" id="' +
 		index +
 		'"><div class="row no-gutters input-group"><div class="col-sm col-lg-1 input-group-prepend hour justify-content-sm-end pr-3 pt-3">' +
 		timeLabel +
@@ -130,11 +55,11 @@ planWorkday.forEach(function(timeBlock, index) {
 		timeBlock.event +
 		'</textarea><div class="col-sm col-lg-1 input-group-append"><button class="saveBtn btn-block" type="submit"><i class="fas fa-save"></i></button></div></div></div>';
 
-
+	/* Adding rows to container div */
 	$(".container").append(row);
 });
 
-
+/* Color rows based on current time */
 function colorRow(time) {
 	var planNow = moment(now, "H A");
 	var planEntry = moment(time, "H A");
@@ -147,14 +72,14 @@ function colorRow(time) {
 	}
 }
 
-
+/* Save Events */
 $(".saveBtn").on("click", function() {
 	var blockID = parseInt(
 		$(this)
 			.closest(".time-block")
 			.attr("id")
 	);
-    var userEntry = $.trim(
+	var userEntry = $.trim(
 		$(this)
 			.parent()
 			.siblings("textarea")
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Module-5-work-day-scheduler-
 HW#5
+# User Story
+I want the abilty to schedule events by hour in one work day
+<br>
+current hour should be displayed in red
+<br>
+all previous hours should be grayed out
+<br>
+all future hours should be green & have the ability to save for future event
+
+#TODO
+Add code for the up to date time & day
+<br>
+Add rows for the hours between 9am and 5pm
+<br>
+
+#CODE
+
+used jquery for CSS & HTML & added them to JS
+
+## Links to my github
+
+
+## Link to my REPO
+
+##
